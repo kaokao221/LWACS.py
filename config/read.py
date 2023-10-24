f3485b67cf1f76d067faec9061b2580870e32cfa
kaokao221 <kaokao221@outlook.com>
2023-10-24 23:39:46 +0800

    Little changes
    Some Changes with mian.py, only for saving.

--- config/read.py
+++ config/read.py
@@ -5,9 +5,10 @@ path = os.path.dirname(os.path.abspath(__file__))
 
 
 class Config:
-    metadata = open(file=f"{path}\\config.json", mode="r", encoding="utf-8").read()
-    with open(file=f"{path}\\config.json", mode="r", encoding="utf-8") as meta:
-        data = json.load(meta)
+    def __init__(self):
+        self.metadata = open(file=f"{path}\\config.json", mode="r", encoding="utf-8").read()
+        with open(file=f"{path}\\config.json", mode="r", encoding="utf-8") as meta:
+            self.data = json.load(meta)
 
     def getdata(self) -> dict:
         return self.data
--- main.py
+++ main.py
@@ -1,38 +1,48 @@
+"""
+LiteWearable AppGallery, Community and Self-signed Platform
+"""
+
 # import AppGalleryConnect
 import uvicorn
 # import config.read
 # import config.edit
 # import config.management
-from fastapi import (FastAPI, Response, Request)
+from fastapi import (FastAPI, Response, status)
 from fastapi.responses import *
+
+import config.read
 
 # print(config.read.Config.getdata)
 
 
-main = FastAPI()
+LWACS = FastAPI()
 
 
-@main.get(path="/")
-async def root(command_code: str | None = None, response=Response, requests=Request):
-    """
-    Which is the root application
-    :param command_code:
-    :param response:
-    :param requests:
-    :return:
-    """
-    response.status_code = 301
-    response.redirect = "/main/"
-    url = "/main"
+@LWACS.get(path="/")
+async def docs_redirect(response=Response):
+    response.status_code = status.HTTP_301_MOVED_PERMANENTLY
+    url = "/docs"
     return RedirectResponse(url=url)
 
 
-@main.post(path="/")
-async def root(command_code)
+@LWACS.get(path="/account/create")
+async def create_account(UUID: str, response=Response) -> Any:
+    """
+    用于注册一个新账户，账户管理用于社区而不是用于应用签名，该步骤不能省略
+    :param UUID: 设备UUID
+    :return: 账号的UID
+    """
+    conf = config.read.Config().getdata()
+    for uid in conf["accounts"]:
+        for did in conf["accounts"][uid]["devices"]:
+            if conf["accounts"][uid]["devices"][did]["UUID"] == UUID:
+                response.statue_code = status.HTTP_301_MOVED_PERMANENTLY
+                url = f"/account/get_uid?UUID={UUID}"
+                return RedirectResponse(url=url)
 
 if __name__ == '__main__':
     uvicorn.run(
-        app=main,
+        app=LWACS,
         host="127.0.0.1",
         port=8899
     )
